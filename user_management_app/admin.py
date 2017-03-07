f6a24f4a5b517d59e0e5c6dd7e873f3ae1b4a317
Rohan Agarwal <ragarwal.consultant@videologygroup.com>
2017-03-07 11:51:24 +0530

    Revert "Adding code to master - first time"
    This reverts commit b9e94a6995df2560a55d687af9a5831880fd6862.

--- user_management_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from user_management_app.models import User
-
-admin.site.register(User)
--- user_management_app/validators.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.exceptions import ValidationError
-
-
-def validate_adult(value):
-    if value < 18:
-        raise ValidationError('Age should be greater than 18 years')
